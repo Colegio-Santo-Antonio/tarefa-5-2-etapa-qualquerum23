8a03a071c1f4c678e8f7ae0a28840458ea57a4d7
FlavioSousaTostes <csa16-6223@colegiosantoantonio.com.br>
2025-08-27 16:59:53 -0300

    Update student.py

--- student.py
+++ student.py
@@ -3,15 +3,15 @@ impares = []
 for i in numero[-1::-2]:
   impares.append(int(1))
 pares = []
-for i in numeros[-2::-2]:
+for i in numero[-2::-2]:
   if 2*int(i)<10:
     pares.append(2*int(i))
   else:
     pares.append(2*int(i) - 10+1)
-  somatório = sum(pares) + sum (ímpares)
+somatório = sum(pares) + sum (ímpares)
   if int(somatório/10) == somatório/10:
     print("Cartão Válido")
-    else: 
+   else: 
       print ("Cartão inválido")
   
         
